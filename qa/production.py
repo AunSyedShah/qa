2229b24214f68cc27d4509de9310eddafeaf2a45
aunsyedshah <aunsyedsyed@gmail.com>
2021-06-16 15:50:09 +0500

    update productions

--- qa/production.py
+++ qa/production.py
@@ -3,7 +3,7 @@ import os
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = ["qavu.azurewebsites.net"]
 
 # WhiteNoise configuration
 MIDDLEWARE = [
@@ -17,7 +17,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # DBHOST is only the server name, not the full URL
 hostname = os.environ['DBHOST']

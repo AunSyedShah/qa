7596b57108f3101f8565af72ea9fd8d043859226
aunsyedshah <aunsyedsyed@gmail.com>
2021-06-17 15:57:48 +0500

    admin css fix try 1

--- qa/settings.py
+++ qa/settings.py
@@ -120,5 +120,4 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
